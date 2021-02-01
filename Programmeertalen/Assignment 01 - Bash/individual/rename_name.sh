cab61129c401359e60d9f2694446640781c02521
René Kok <rkok98@gmail.com>
2021-02-01 22:51:14 +0100

    Rename name

--- Programmeertalen/Assignment 01 - Bash/individual/rename_name.sh
+++ Programmeertalen/Assignment 01 - Bash/individual/rename_name.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+#
+# Author:   René Kok (13671146)
+# Study:    Doorstroomminor Software Engineering UvA
+# 
+# This script prints the file name and line number of each occurrence of
+# a given string in all the Java source files in the repository.
+
+readonly START_DIRECTORY=./..
+readonly SOURCE_SUFFIX=*.java
+
+find $START_DIRECTORY -name \*$SOURCE_SUFFIX -type f -exec \
+    sed -i '' -e 's/'$1'/'$2'/g' {} + 
