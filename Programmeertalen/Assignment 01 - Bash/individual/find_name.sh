d59e241298609251d6de2a23ff3df0df2fceb7c4
René Kok <rkok98@gmail.com>
2021-02-01 14:48:36 +0100

    Find Name

--- Programmeertalen/Assignment 01 - Bash/individual/find_name.sh
+++ Programmeertalen/Assignment 01 - Bash/individual/find_name.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#
+# Author:   René Kok (13671146)
+# Study:    Doorstroomminor Software Engineering UvA
+# 
+# This script prints the file name and line number of each occurrence of
+# a given string in all the Java source files in the repository.
+
+readonly START_DIRECTORY=./..
+readonly SOURCE_SUFFIX=*.java
+
+grep -niro --include=\*$SOURCE_SUFFIX $1 $START_DIRECTORY
