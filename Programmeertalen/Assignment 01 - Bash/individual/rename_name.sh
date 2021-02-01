f8cd443c60686b99b2b589ffa69b0d1580f7a1be
René Kok <rkok98@gmail.com>
2021-02-01 23:01:56 +0100

    Updated rename_name with comments

--- Programmeertalen/Assignment 01 - Bash/individual/rename_name.sh
+++ Programmeertalen/Assignment 01 - Bash/individual/rename_name.sh
@@ -3,8 +3,9 @@
 # Author:   René Kok (13671146)
 # Study:    Doorstroomminor Software Engineering UvA
 # 
-# This script prints the file name and line number of each occurrence of
-# a given string in all the Java source files in the repository.
+# Usage:    ./rename_name.sh <name-old> <name-new>
+#
+# This script replaces given names with new given names.
 
 readonly START_DIRECTORY=./..
 readonly SOURCE_SUFFIX=*.java
@@ -12,5 +13,7 @@ readonly SOURCE_SUFFIX=*.java
 readonly OLD_NAME=$1
 readonly NEW_NAME=$2
 
+# The 'find' program recursively searches the start directory for java files, 
+# in which the 'sed' application replaces the old names with the new names.
 find $START_DIRECTORY -name \*$SOURCE_SUFFIX -type f -exec \
     sed -i '' -e 's/'$OLD_NAME'/'$NEW_NAME'/g' {} + 
