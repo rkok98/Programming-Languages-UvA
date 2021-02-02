144046fc80cff66e60c9ed2bc828c4ae5056a67d
René Kok <rkok98@gmail.com>
2021-02-02 22:22:56 +0100

    Rename Class file

--- Programmeertalen/Assignment 01 - Bash/individual/rename_class.sh
+++ Programmeertalen/Assignment 01 - Bash/individual/rename_class.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+#
+# Author:   René Kok (13671146)
+# Study:    Doorstroomminor Software Engineering UvA
+# 
+# Usage:    ./rename_name.sh <name-old> <name-new>
+#
+# This script replaces given names with new given names.
+
+readonly START_DIRECTORY=./..
+readonly SOURCE_SUFFIX=.java
+
+readonly OLD_CLASS=$1
+readonly NEW_CLASS=$2
+
+find $START_DIRECTORY -name $OLD_CLASS$SOURCE_SUFFIX  -exec mv {} $NEW_CLASS$SOURCE_SUFFIX
+
