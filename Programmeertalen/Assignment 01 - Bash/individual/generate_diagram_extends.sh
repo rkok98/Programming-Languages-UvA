f03e2446c3bf8e58bb28eea20b902bb13b1efe1a
René Kok <rkok98@gmail.com>
2021-02-03 21:23:53 +0100

    Extend diagram

--- Programmeertalen/Assignment 01 - Bash/individual/generate_diagram_extends.sh
+++ Programmeertalen/Assignment 01 - Bash/individual/generate_diagram_extends.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+echo "digraph D {"
+for f in $( find .. -name '*.java')
+do    
+    cat $f | ./remove_comments.sh | perl -0ne 'while (/class\s+(\S+)\s+extends\s+(\S+).*\{.*}/sg) {print "$1 -> $2\n$1 [shape=oval]\n";}'
+done
+echo "}"
