c8bbb6b764cfddda7346e600d83c55d01f309441
René Kok <rkok98@gmail.com>
2021-02-05 16:53:16 +0100

    Started working on import diagrams

--- Programmeertalen/Assignment 01 - Bash/individual/generate_diagram_import.sh
+++ Programmeertalen/Assignment 01 - Bash/individual/generate_diagram_import.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+JAVAS=$( find .. -name '*.java')
+
+for f in JAVAS
+do    
+    cat $f | ./remove_comments.sh | perl -0ne 'while (/class\s+(\S+)\s+extends\s+(\S+).*\{.*}/sg) {print "$1 -> $2\n$1 [shape=oval]\n";}'
+done
