4a0603f6cbf5448596cbc7ff0d234ebfb50287e8
René Kok <rkok98@gmail.com>
2021-02-06 14:08:40 +0100

    Generate diagram packages

--- Programmeertalen/Assignment 01 - Bash/individual/generate_diagram_package.sh
+++ Programmeertalen/Assignment 01 - Bash/individual/generate_diagram_package.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+# package\s(\S+)\;
+
+JAVAS=$( find .. -name '*.java')
+
+packages=$(mktemp -d)
+
+for f in $JAVAS
+do  
+    source=$(cat $f | ./remove_comments.sh)
+    
+    package=$(echo $source | perl -0ne 'while (/package\s(\S+)\;/sg) {print "$1\n";}')
+    class=$(echo $source | perl -0ne 'while (/class\s+(\S+).*?\{.*}/sg) {print "$1\n";}')
+
+    if [ -n "$package" ]; then
+        echo $class >> $packages/$package
+    fi
+done
+
+echo "digraph D {"
+for f in $packages/*
+do
+    package=$(basename $f)
+
+    echo -e "\tsubgraph cluster_X { label = "$package""
+    for l in $(cat $f)
+    do
+        echo -e "\t\t"$l" [shape=box]"
+    done
+    echo -e "\t}\n"
+done
+echo "}"
