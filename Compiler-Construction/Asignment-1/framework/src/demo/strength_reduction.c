c4fac63b954e38e3c6c1ce0e9dadb6616e88805b
René Kok <rkok98@gmail.com>
2021-02-08 16:15:26 +0100

    Initialized binOp

--- Compiler-Construction/Asignment-1/framework/src/demo/strength_reduction.c
+++ Compiler-Construction/Asignment-1/framework/src/demo/strength_reduction.c
@@ -23,39 +23,51 @@
 #include "free.h"
 #include "str.h"
 
-
-
 /*
  * Traversal functions
  */
 
-node *SRbinop (node *arg_node, info *arg_info)
-{
+node *SRbinop(node *arg_node, info *arg_info) {
     DBUG_ENTER("SRbinop");
 
     /*
      * Extremely important:
      *  we must continue to traverse the abstract syntax tree !!
      */
-    BINOP_LEFT( arg_node) = TRAVdo( BINOP_LEFT( arg_node), arg_info);
-    BINOP_RIGHT( arg_node) = TRAVdo( BINOP_RIGHT( arg_node), arg_info);
+    BINOP_RIGHT(arg_node) = TRAVdo(BINOP_RIGHT(arg_node), arg_info);
+    BINOP_LEFT(arg_node) = TRAVdo(BINOP_LEFT(arg_node), arg_info);
 
-    if (BINOP_OP( arg_node) == BO_sub) {
-        if ((NODE_TYPE( BINOP_LEFT( arg_node)) == N_var)
-            && (NODE_TYPE( BINOP_RIGHT( arg_node)) == N_var)
-            && STReq( VAR_NAME( BINOP_LEFT( arg_node)), VAR_NAME( BINOP_RIGHT( arg_node)))) {
-            arg_node = FREEdoFreeTree( arg_node);
-            arg_node = TBmakeNum( 0);
+    // 1. Check if operator is a multiplier operator
+    if (BINOP_OP(arg_node) == BO_mul) {
+        // 2. Declare var node and multiplier node
+        int MULTIPLIER;
+        node * VAR_NODE;
+
+        // Writes var and multiplier to the right node
+        // Returns if there's no num and var node
+        if ((NODE_TYPE(BINOP_LEFT(arg_node)) == N_var)
+            && (NODE_TYPE(BINOP_RIGHT(arg_node)) == N_num)) {
+            MULTIPLIER = NUM_VALUE(BINOP_RIGHT(arg_node)) - 1;
+            VAR_NODE = BINOP_LEFT(arg_node);
+        } else if ((NODE_TYPE(BINOP_LEFT(arg_node)) == N_num)
+                   && (NODE_TYPE(BINOP_RIGHT(arg_node)) == N_var)) {
+            MULTIPLIER = NUM_VALUE(BINOP_LEFT(arg_node)) - 1;
+            VAR_NODE = BINOP_RIGHT(arg_node);
+        } else {
+            DBUG_RETURN(arg_node);
         }
-        else if  ((NODE_TYPE( BINOP_LEFT( arg_node)) == N_num)
-                  && (NODE_TYPE( BINOP_RIGHT( arg_node)) == N_num)
-                  && (NUM_VALUE( BINOP_LEFT( arg_node)) == NUM_VALUE( BINOP_RIGHT( arg_node)))) {
-            arg_node = FREEdoFreeTree( arg_node);
-            arg_node = TBmakeNum( 0);
-        }
+
+        // 1. Make Parent addition
+        BINOP_OP(arg_node) = BO_add;
+
+        // 2. Make right node the var
+        BINOP_RIGHT(arg_node) = VAR_NODE;
+
+        // 3. Make left node var * multiplier
+        BINOP_LEFT(arg_node) = TBmakeBinop(BO_mul, TBmakeNum(2), TBmakeNum(MULTIPLIER));
     }
 
-    DBUG_RETURN( arg_node);
+    DBUG_RETURN(arg_node);
 }
 
 
@@ -63,13 +75,12 @@ node *SRbinop (node *arg_node, info *arg_info)
  * Traversal start function
  */
 
-node *SRdoStrengthReduction( node *syntaxtree)
-{
+node *SRdoStrengthReduction(node *syntaxtree) {
     DBUG_ENTER("SRdoStrengthReduction");
 
-    TRAVpush( TR_sr);
-    syntaxtree = TRAVdo( syntaxtree, NULL);
+    TRAVpush(TR_sr);
+    syntaxtree = TRAVdo(syntaxtree, NULL);
     TRAVpop();
 
-    DBUG_RETURN( syntaxtree);
+    DBUG_RETURN(syntaxtree);
 }
