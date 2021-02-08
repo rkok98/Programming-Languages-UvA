49f24af37838b9e62253d0bd50e1c46691112a8d
René Kok <rkok98@gmail.com>
2021-02-08 21:28:27 +0100

    Haskell

---
+++ Programmeertalen/Assignment 02 - Haskell/sudoku/SudokuSolver.hs
@@ -0,0 +1,62 @@
+import System.Environment
+import Data.List
+
+type Row = Int
+type Column = Int
+type Value = Int
+type Grid = [[Value]]
+type Sudoku = (Row,Column) -> Value
+type Constraint = (Row, Column, [Value])
+type Node = (Sudoku, [Constraint])
+
+positions :: [Int]
+positions = [1..9]
+
+values :: [Value]
+values = [1..9]
+
+blocks :: [[Int]]
+blocks = [[1..3],[4..6],[7..9]]
+
+centerOfBlocks :: [Int]
+centerOfBlocks = [1, 4, 7]
+
+-- Converts a sudoku into a grid.
+sud2grid :: Sudoku -> Grid
+sud2grid s = [[s (r, c) | c <- positions] | r <- positions]
+
+-- Converts a grid into a sudoku.
+grid2sud :: Grid -> Sudoku
+grid2sud gr = \(r, c) -> pos gr (r, c)
+  where pos :: [[a]] -> (Row,Column) -> a
+        pos gr (r, c) = (gr !! (r - 1)) !! (c - 1)
+
+-- Function to extend a sudoku with a value at (row, column).
+extend :: Sudoku -> (Row, Column, Value) -> Sudoku
+extend sud (r, c, v) (i, j) = if r == i && c == j then v else sud (i, j)
+
+
+-- Read a file-sudoku into a Sudoku
+readSudoku :: String -> IO Sudoku
+readSudoku filename =
+    do stringGrid <- readFile filename
+       return $ (grid2sud . splitStringIntoGrid) stringGrid
+       where splitStringIntoGrid = map (map readint . words) . lines
+             readint x = read x :: Int
+
+-- Prints a Sudoku to the terminal by transforming it to a grid first.
+printSudoku :: Sudoku -> IO ()
+printSudoku = putStr . unlines . map (unwords . map show) . sud2grid
+
+-- Helper to parse command-line arguments.
+getSudokuName :: [String] -> String
+getSudokuName [] = error "Filename of sudoku as first argument."
+getSudokuName (x:_) = x
+
+-- Do not modify the way your sudoku is printed!
+main =
+    do args <- getArgs
+       sud <- (readSudoku . getSudokuName) args
+       -- TODO: Call your solver.
+       printSudoku sud
+
