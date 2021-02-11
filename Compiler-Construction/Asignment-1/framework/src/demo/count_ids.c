9c696db07ad85e995afa9db23356e15e7b6121eb
René Kok <rkok98@gmail.com>
2021-02-11 21:52:34 +0100

    Created countd ids

--- Compiler-Construction/Asignment-1/framework/src/demo/count_ids.c
+++ Compiler-Construction/Asignment-1/framework/src/demo/count_ids.c
@@ -9,14 +9,22 @@
 #include "memory.h"
 #include "ctinfo.h"
 
-#include "stdio.h"
-
+#include "str.h"
 
 struct INFO {
     lut_t *table;
 };
 
+struct COUNTER {
+    int amount;
+    char *id;
+};
+
+typedef struct COUNTER counter;
+
 #define INFO_LUT(n)  ((n)->table)
+#define COUNTER_AMOUNT(n) ((n)->amount)
+#define COUNTER_ID(n) ((n)->id)
 
 static info *MakeInfo(void) {
     info *result;
@@ -38,20 +46,55 @@ static info *FreeInfo(info *info) {
     DBUG_RETURN(info);
 }
 
+static void *PrintLut(void *item) {
+    DBUG_ENTER("printLut");
+
+    counter *c = (counter *) item;
+    CTInote("Identifier %s occurs %d time(s)", COUNTER_ID(c), COUNTER_AMOUNT(c));
+
+    DBUG_RETURN(item);
+}
+
+static void *InsertCounter(lut_t *LUT, char *id) {
+    DBUG_ENTER("Insert");
+
+    void **S = LUTsearchInLutS(LUT, id);
+
+    if (S != NULL) {
+        counter *counter = *S;
+        COUNTER_AMOUNT(counter) += 1;
+    } else {
+        counter *c = (counter *) MEMmalloc(sizeof(counter));
+
+        COUNTER_AMOUNT(c) = 1;
+        COUNTER_ID(c) = id;
+
+        LUTinsertIntoLutS(LUT, id, c);
+    }
+
+    DBUG_RETURN(id);
+}
+
 node *CIDvar(node *arg_node, info *arg_info) {
     DBUG_ENTER("CIDvar");
-    /*
-    if (LUTsearchInLutS(STRcpy(VAR_NAME(arg_node))) != NULL) {
-        printf("AAAA");
-    }
-     */
 
-    printf("BBBB");
+    lut_t *LUT = INFO_LUT(arg_info);
+    char *ID = STRcpy(VAR_NAME(arg_node));
+
+    InsertCounter(LUT, ID);
+
     DBUG_RETURN(arg_node);
 }
 
+
 node *CIDvarlet(node *arg_node, info *arg_info) {
     DBUG_ENTER("CIDvarlet");
+
+    lut_t *LUT = INFO_LUT(arg_info);
+    char *ID = STRcpy(VARLET_NAME(arg_node));
+
+    InsertCounter(LUT, ID);
+    
     DBUG_RETURN(arg_node);
 }
 
@@ -65,6 +108,9 @@ node *CIDdoCountIds(node *syntaxtree) {
     TRAVpush(TR_cid);
     syntaxtree = TRAVdo(syntaxtree, arg_info);
     TRAVpop();
+
+    lut_t *LUT = INFO_LUT(arg_info);
+    LUTmapLutS(LUT, PrintLut);
 
     arg_info = FreeInfo(arg_info);
 
--- Compiler-Construction/Asignment-1/framework/src/demo/count_ids.h
+++ Compiler-Construction/Asignment-1/framework/src/demo/count_ids.h
@@ -1,8 +1,10 @@
 #ifndef _COUNT_IDS_H
 #define _COUNT_IDS_H
 
-extern node *CIDvar (node *arg_node, info *arg_info);
-extern node *CIDvarlet (node *arg_node, info *arg_info);
-extern node *CIDdoCountIds( node *syntaxtree);
+extern node *CIDvar(node *arg_node, info *arg_info);
 
-#endif //PRE_MASTER_COUNT_IDS_H
+extern node *CIDvarlet(node *arg_node, info *arg_info);
+
+extern node *CIDdoCountIds(node *syntaxtree);
+
+#endif //_COUNT_IDS_H
