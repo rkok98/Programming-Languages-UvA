fc95a5b70aea6ec73deb1711f26c7a16b92f8553
René Kok <rkok98@gmail.com>
2021-02-13 18:10:37 +0100

    Comments and small rewrite

--- Programmeertalen/Assignment 02 - Haskell/sudoku/SudokuSolver.hs
+++ Programmeertalen/Assignment 02 - Haskell/sudoku/SudokuSolver.hs
@@ -1,3 +1,8 @@
+-- Author:   René Kok (13671146)
+-- Study:    Doorstroomminor Software Engineering UvA
+-- 
+-- This script solves simple sudoku's by using a search tree.
+
 import System.Environment
 import Data.List
 import Data.Maybe
@@ -20,7 +25,7 @@ blocks :: [[Int]]
 blocks = [[1..3],[4..6],[7..9]]
 
 centerOfBlocks :: [Int]
-centerOfBlocks = [1, 4, 7]
+centerOfBlocks = [2, 5, 8]
 
 openPosition :: Int
 openPosition = 0
@@ -61,54 +66,44 @@ getSudokuName (x:_) = x
 main =
     do args <- getArgs
        sud <- (readSudoku . getSudokuName) args
-       -- TODO: Call your solver.
-       
-       -- print $ consistent sud
-
-       -- print "Non-consistent Sudoku:" 
-       -- printSudoku sud
-
-       -- print "Consistent Sudoku:"
        printSudoku (solveSudoku sud)
 
-freeInLs :: [Value] -> [Value]
-freeInLs ls = values \\ ls 
-
-subGrid :: Sudoku -> (Row,Column) -> [Value]
-subGrid s (r,c) = 
-  [ s (r',c') | r' <- (\x -> concat $ filter (elem x) blocks ) r, 
+subGrid :: Sudoku -> (Row, Column) -> [Value]
+subGrid s (r,c) = [ s (r',c') | r' <- (\x -> concat $ filter (elem x) blocks ) r, 
                 c' <- (\x -> concat $ filter (elem x) blocks ) c ]
 
 freeInRow :: Sudoku -> Row -> [Value]
-freeInRow s r = freeInLs [ s (r,i) | i <- positions ]
+freeInRow sud row = values \\ [ sud (row, pos) | pos <- positions ]
 
 freeInColumn :: Sudoku -> Column -> [Value]
-freeInColumn s c = freeInLs [ s (i,c) | i <- positions ]
+freeInColumn sud col = values \\ [ sud (pos, col) | pos <- positions ]
 
-freeInSubgrid :: Sudoku -> (Row,Column) -> [Value]
-freeInSubgrid s (r,c) = freeInLs (subGrid s (r,c))
+freeInSubgrid :: Sudoku -> (Row, Column) -> [Value]
+freeInSubgrid sud (row, col) = values \\ (subGrid sud (row, col))
 
-freeAtPos :: Sudoku -> (Row,Column) -> [Value]
-freeAtPos s (r,c) = freeInRow s r `intersect` freeInColumn s c `intersect` freeInSubgrid s (r,c)
+freeAtPos :: Sudoku -> (Row, Column) -> [Value]
+freeAtPos sud (row, col) = freeInRow sud row `intersect` 
+                           freeInColumn sud col `intersect` 
+                           freeInSubgrid sud (row, col)
 
-openPositions :: Sudoku -> [(Row,Column)]
-openPositions s = [ (r,c) | r <- positions,  
-                            c <- positions, 
-                            s (r,c) == openPosition ]
+openPositions :: Sudoku -> [(Row, Column)]
+openPositions sud = [ (row, col) | row <- positions,  
+                                   col <- positions, 
+                                   sud (row, col) == openPosition ]
 
 rowValid :: Sudoku -> Row -> Bool
-rowValid s r = null (freeInRow s r)
+rowValid sud row = null (freeInRow sud row)
 
 colValid :: Sudoku -> Column -> Bool
-colValid s c = null (freeInColumn s c)
+colValid sud col = null (freeInColumn sud col)
 
-subgridValid :: Sudoku -> (Row,Column) -> Bool
-subgridValid s (r,c) = null (freeInSubgrid s (r,c))
+subgridValid :: Sudoku -> (Row, Column) -> Bool
+subgridValid sud (row, col) = null (freeInSubgrid sud (row, col))
 
 consistent :: Sudoku -> Bool
-consistent s = and $ [ rowValid s r | r <- positions ] ++ [ colValid s c | c <- positions] ++ [ subgridValid s (r,c) | r <- positions, c <- positions ]
-
--- Solver part
+consistent sud = and $ [ rowValid sud row | row <- positions ] ++ 
+                       [ colValid sud col | col <- positions] ++ 
+                       [ subgridValid sud (row, col) | row <- positions, col <- positions ]
 
 -- Prints a node
 printNode :: Node -> IO() 
@@ -116,28 +111,20 @@ printNode = printSudoku . fst
 
 -- Calculates all constraints for a given sudoku sorted by solutions length
 constraints :: Sudoku -> [Constraint]
-constraints s = sortBy solutionsLengthComparable [(r, c, freeAtPos s (r,c)) | (r,c) <- openPositions s ]
+constraints sud = sortBy solutionsLengthComparable [(row, col, freeAtPos sud (row, col)) | (row, col) <- openPositions sud ]
 
 -- Comparable that compares the length of the solutions array in a constraint
 solutionsLengthComparable :: Constraint -> Constraint -> Ordering
-solutionsLengthComparable (_, _, sols) (_, _, sols') = compare (length sols) (length sols')
+solutionsLengthComparable (_, _, solutions) (_, _, solutions') = compare (length solutions) (length solutions')
 
-
--- makeTree :: [Constraint] -> Tree
-
--- 1. Make binary tree from list
--- 2. Traverse through binary tree
- 
 solveSudoku :: Sudoku -> Sudoku
 solveSudoku sud = solve (sud, constraints sud)
 
 solve :: Node -> Sudoku
-solve (s, []) | consistent s = s
-              | otherwise = s -- WTF MOET IK HIER DOEN
-solve (s, cs) = solve (extendNode (s, tail cs) (r, c, head v))
-  where (r, c, v) = head cs
+solve (sud, []) | consistent sud = sud
+                | otherwise = sud 
+solve (sud, cons) = solve (extendNode (sud, tail cons) (row, col, head v))
+  where (row, col, v) = head cons
 
--- data Node = (Sudoku, [Constraint])
--- extend :: Sudoku -> (Row, Column, Value) -> Sudoku
 extendNode :: Node -> (Row, Column, Value) -> Node
-extendNode (s, cs) (r, c, v) = (extend s (r, c, v), cs)
+extendNode (sud, cons) (row, col, con) = (extend sud (row, col, con), cons)
