dd368ab11813b0ed076b63cf970d1970253abb58
René Kok <rkok98@gmail.com>
2021-02-22 10:11:25 +0100

    print sudoku

--- Programmeertalen/Assignment 04 - Python/sudoku/sudoku.py
+++ Programmeertalen/Assignment 04 - Python/sudoku/sudoku.py
@@ -9,21 +9,38 @@ def sudokuToArray(filename):
 
         for row in lines:
             row = row.rstrip()
-            sudoku.append(row.split(' '))
+            row = list(map(int, row.split(' ')))
+            sudoku.append(row)
 
     size = len(sudoku)
 
     return sudoku, size
 
+# Prints sudoku
+def printSudoku(sudoku):
+    for _, row in enumerate(sudoku):
+        print(' '.join(map(str, row)))
 
-def freeValues(values, n):
+def freeValues(values):
+    n = [item for item in range(1, len(values))] 
+
     values_set = set(values)
     n_set = set(n)
 
     return (values_set - n_set).union(n_set - values_set)
 
+def freeInRow(sudoku, row):
+    return freeValues(sudoku[row])
+
+
+
 def main():
     print("hello world!")
 
 if __name__ == "__main__":
-    sudokuToArray('/Users/renekok/Developer/pre-master/Programmeertalen/Assignment 04 - Python/sudoku/sudoku_boards/1_open_spots_9_grid.txt')
+    sudoku, size = sudokuToArray('/Users/renekok/Developer/pre-master/Programmeertalen/Assignment 04 - Python/sudoku/sudoku_boards/1_open_spots_9_grid.txt')
+    print(sudoku)
+    printSudoku(sudoku)
+    print(freeInRow(sudoku, 1))
+
+
