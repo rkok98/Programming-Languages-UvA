af344c41a2a36e64f2f33a97052f648dc2c03f63
René Kok <rkok98@gmail.com>
2021-03-01 16:52:50 +0100

    Init

---
+++ Programmeertalen/Assignment 05 - Erlang/opgave/tests_2b.erl
@@ -0,0 +1,82 @@
+-module(tests_2b).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([tests_run/0]).
+
+-import(client, [choose_random_wall/1, new/0]).
+-import(mock_game_server, [start_link/1]).
+
+tests_run() ->
+    halt(case eunit:test(?MODULE) of
+        ok -> 0;
+        _ -> 1
+    end).
+
+get_open_spots_test_() ->
+    Walls = [{{-1,0},{0,0}},
+         {{0,-1},{0,0}},
+         {{0,0},{0,1}},
+         {{0,0},{1,0}},
+         {{1,-1},{1,0}},
+         {{1,0},{1,1}},
+         {{1,0},{2,0}}
+        ],
+    [H|T] = Walls,
+
+    [?_assertEqual(H, client:choose_random_wall({2, 1, T})),
+     ?_assertEqual([], client:choose_random_wall({2, 1, [H|T]}))
+    ].
+
+setup() ->
+    Client = client:new(),
+    {ok, MockServer} = mock_game_server:start_link({1, 1, []}),
+    {MockServer, Client}.
+
+
+shutdown({_Server, Client}) ->
+    exit(Client, normal),
+    ok.
+
+% Safety check.
+test_spawn({_, Client}) ->
+    ?assert(is_process_alive(Client)).
+
+
+wait_for_move(Server, Last) ->
+    {ok, Wall} = gen_server:call(Server, last_wall),
+    if 
+        Wall == Last -> wait_for_move(Server, Last);
+        true -> Wall
+    end.
+   
+% Tests if you respond well to a move request.
+test_move({Server, Client}) ->
+    % Grid of 1 Cell.
+    Walls = [{{-1,0},{0,0}},{{0,-1},{0,0}},{{0,0},{0,1}},{{0,0},{1,0}}],
+    [H|T] = Walls,
+    Client ! {move, Server, {1, 1, T}},
+    Wall = wait_for_move(Server, nil),
+    ?assertEqual(Wall, H),
+    Client ! {move, Server, {1, 1, [H]}},
+    NewWall = wait_for_move(Server, Wall),
+    ?assertNotEqual(H, NewWall).
+
+
+instantiator(Fixture) ->
+    % You have ten seconds(more than enough).
+    % If you get a timeout, your client is not
+    % making moves.
+    {timeout, 10, {inorder, 
+        [?_test(test_spawn(Fixture)),
+         ?_test(test_move(Fixture))
+        ]
+    }}.
+
+
+move_test_() ->
+    {setup, 
+        fun setup/0,  
+        fun shutdown/1,
+        fun instantiator/1
+    }.
