6a32d4b13218b0d0ecc22586e998a316767ffbed
René Kok <34970901+rkok98@users.noreply.github.com>
2021-03-06 13:20:41 +0100

    Programmeertalen/erlang (#5)
    * Init
    
    * New method
    
    * Ontwikkelend
    
    * Ontwikkelend
    
    * List
    
    * Drawing walls
    
    * Working print
    
    * Cleanup
    
    * 2a
    
    * 2a
    
    * client
    
    * Gevorderd
    
    * KK
    
    * Random wall
    
    * Working
    
    * Score
    
    * Gevorderd
    
    * Comments
    
    * Cleanup

---
+++ Programmeertalen/Assignment 05 - Erlang/opgave/client.erl
@@ -0,0 +1,18 @@
+-module(client).
+-export([move/0, new/0]).
+
+
+move() ->
+    <<S1:32, S2:32, S3:32>> = crypto:strong_rand_bytes(12),
+    rand:seed(exs1024,{S1, S2, S3}),
+    receive
+        finished ->
+            io:format("~p: I am done~n", [self()]);
+        {move, ServerPid, Grid} ->
+            Wall = grid:choose_random_wall(Grid),
+            game_server:move(ServerPid, Wall),
+            move()
+    end.
+
+new() ->
+    spawn(client, move, []).
---
+++ Programmeertalen/Assignment 05 - Erlang/opgave/game_server.erl
@@ -0,0 +1,82 @@
+-module(game_server).
+
+-behaviour(gen_server).
+
+-export([start_link/1, handle_call/3, handle_cast/2]).
+-export([init/1, move/2, handle_continue/2]).
+
+start_link({W, H, Players}) ->
+    gen_server:start_link(game_server, {W, H, Players}, []).
+
+% Abstraction to make a move.
+move(Pid, Wall) ->
+    gen_server:call(Pid, {move, Wall}).
+
+% Informs the players that the game has finished.
+finished([]) ->
+    ok;
+finished(Players) -> 
+    [H | T] = Players,
+    H ! finished,
+    finished(T).
+
+% Calculates the score of an added wall.
+calculate_score(Wall, Grid) -> 
+    {Cell1, Cell2} = Wall,
+    cell_score(Cell1, Grid) + cell_score(Cell2, Grid).
+
+% Calculates the score of a cell.
+cell_score(Cell, Grid) ->
+    case grid:is_closed(Cell, Grid) of
+        true ->
+            1;
+        false ->
+            0
+    end.
+
+% Decides who is the next player
+decide_next_player(Players) -> 
+    lists:reverse(Players).
+
+% TODO: You need to inform the first player to move.
+init({Width, Height, Players}) ->
+    Grid = grid:new(Width, Height),
+    State = {Grid, Players},
+    {ok, State, {continue, move}}.
+
+handle_continue(move, State) ->
+    {Grid, Players} = State,
+    case grid:filled(Grid) of
+        true ->
+            finished(Players),
+            {stop, normal, State};
+        false ->
+            hd(Players) ! {move, self(), Grid},
+            {noreply, State}
+    end.
+
+% Handle move call
+% Adds the new wall to the state and decides the next player.
+% It also calculates the score of the move.
+handle_call({move, Wall}, _From, State) ->
+    {Grid, Players} = State,
+    
+    NewGrid = grid:add_wall(Wall, Grid),
+    NewPlayers = decide_next_player(Players),
+
+    NewState = {NewGrid, NewPlayers},
+    
+    Score = calculate_score(Wall, NewGrid),
+
+    {reply, {ok, Score}, NewState, {continue, move}};
+% Used for testing.
+handle_call(state, _From, State) ->
+    {reply, {ok, State}, State};
+handle_call({setWalls, Walls}, _From, {{W, H, _}, Players}) ->
+    {reply, ok, {{W, H, Walls}, Players}}.
+
+% Required for gen_server behaviour.
+% Normally you would implement this too,
+% but not required for this assignment.
+handle_cast(_, State) ->
+    {reply, not_implemented, State}.
