2c1641da3571e8cb8781ca8de605dccd8f74c59d
René Kok <rkok98@gmail.com>
2021-03-10 09:58:04 +0100

    Ontwikkelend

--- Programmeertalen/Assignment 06 - Go/skel.go
+++ Programmeertalen/Assignment 06 - Go/skel.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "sync"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 )
 
 const southWall byte = (1 << 0) // The first flag
@@ -13,7 +13,7 @@ const eastWall byte = (1 << 1)  // The second flag
 type Maze [][]byte
 
 type Position struct {
-    Row, Col int
+	Row, Col int
 }
 
 /* EXAMPLE: for a Position pos, there is NO wall north of pos if and only if
@@ -25,44 +25,56 @@ type Position struct {
  */
 
 func readMaze(f *os.File) (maze Maze) {
-    // TODO: handle errors
-    s := bufio.NewScanner(f)
-    for s.Scan() {
-        maze = append(maze, []byte(s.Text()))
-    }
-    return
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		maze = append(maze, []byte(s.Text()))
+	}
+
+	for i, row := range maze {
+		for cell := range row {
+			wall := maze[i][cell]
+
+			if wall < 48 || wall > 51 {
+				log.Fatal("Maze contains invalid characters.")
+			}
+		}
+	}
+
+	return
 }
 
 func solve(maze Maze) (route []Position) {
-    var onceMaze [][]sync.Once
+	// var onceMaze [][]sync.Once
 
-    // Initialize a channel for communication with goroutines
-    // No functional dependency on the size of a buffer is allowed
+	// Initialize a channel for communication with goroutines
+	// No functional dependency on the size of a buffer is allowed
 
-    // Include a closure for the exploration of maze cells in goroutines
-    /* NOTE: The closure will be a short-lived function. Normally, this
-     *       would be considered inefficient. However, achieving  efficient
-     *       concurrency is not the point of this exercise.
-     */
+	// Include a closure for the exploration of maze cells in goroutines
+	/* NOTE: The closure will be a short-lived function. Normally, this
+	 *       would be considered inefficient. However, achieving  efficient
+	 *       concurrency is not the point of this exercise.
+	 */
 
-    // Initialize onceMaze and use it to limit each cell to a single visit
+	// Initialize onceMaze and use it to limit each cell to a single visit
 
-    // Start the exploration of the maze in a goroutine at position {0, 0}
+	// Start the exploration of the maze in a goroutine at position {0, 0}
 
-    // Receive messages from the goroutines and spawn new ones as needed
-    // Do not spawn new goroutines if a way out of the maze has been found
-    // Stop receiving only when no more exploration goroutines are running
+	// Receive messages from the goroutines and spawn new ones as needed
+	// Do not spawn new goroutines if a way out of the maze has been found
+	// Stop receiving only when no more exploration goroutines are running
+
+	return
 }
 
 func main() {
-    // TODO: handle errors
-    f, _ := os.Open(os.Args[1])
-    maze := readMaze(f)
-    for _, pos := range solve(maze) {
-        maze[pos.Row][pos.Col] |= (1 << 2) // The third flag
-    }
-    for _, line := range maze {
-        // TODO: handle errors
-        fmt.Println(string(line))
-    }
+	// TODO: handle errors
+	f, _ := os.Open(os.Args[1])
+	maze := readMaze(f)
+	for _, pos := range solve(maze) {
+		maze[pos.Row][pos.Col] |= (1 << 2) // The third flag
+	}
+	for _, line := range maze {
+		// TODO: handle errors
+		fmt.Println(string(line))
+	}
 }
