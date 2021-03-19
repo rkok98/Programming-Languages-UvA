327d22cfd29085dc3128718da44a151db95b0be7
René Kok <rkok98@gmail.com>
2021-03-19 15:45:22 +0100

    Str.h

--- Programmeertalen/Assignment 7 - C++/Str.h
+++ Programmeertalen/Assignment 7 - C++/Str.h
@@ -5,11 +5,50 @@
 class Str
 {
 public:
+    Str() : m_str{""} {}
 
-    Str()
+    Str(std::string string)
     {
+        m_str = string;
     }
-        
+
+    std::string str() const
+    {
+        return m_str;
+    }
+
+    friend std::istream &operator>>(std::istream &is, Str &string);
+
 private:
     std::string m_str;
 };
+
+std::istream &operator>>(std::istream &is, Str &str)
+{
+    return is >> str.m_str;
+}
+
+std::ostream &operator<<(std::ostream &os, const Str &str)
+{
+    return os << str.str();
+}
+
+Str operator-(const Str& string)
+{
+    return Str("(-" + string.str() + ")");
+}
+
+Str operator+(const Str& str_1,const Str& str_2)
+{
+    return Str("(" + str_1.str() + "+" + str_2.str() + ")");
+}
+
+Str operator-(const Str &str_1, const Str &str_2)
+{
+  return Str("(" + str_1.str() + "-" + str_2.str() + ")");
+}
+
+Str operator*(const Str &str_1, const Str &str_2)
+{
+    return Str("(" + str_1.str() + "*" + str_2.str() + ")");
+}
