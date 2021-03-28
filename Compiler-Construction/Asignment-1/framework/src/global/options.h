d1c8098d798fd648a873dbe7646b9b6bb0de415f
René Kok <rkok98@gmail.com>
2021-03-28 17:36:01 +0200

    Changed repo to programmeertalen

--- Compiler-Construction/Asignment-1/framework/src/global/options.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SAC_OPTIONS_H_
-#define _SAC_OPTIONS_H_
-
-#include "types.h"
-
-extern void OPTcheckOptions( int argc, char **argv);
-
-#endif /* _SAC_OPTIONS_H_ */
--- Compiler-Construction/compiler/src/codegen/gen_byte_code.h
+++
@@ -1,7 +0,0 @@
-#ifndef _GEN_BYTE_CODE_H_
-#define _GEN_BYTE_CODE_H_
-#include "types.h"
-
-extern node *GBCdoGenByteCode( node *syntaxtree);
-
-#endif
